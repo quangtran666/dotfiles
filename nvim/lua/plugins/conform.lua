a5df1ebb76697b25cc1e145ef54fa875dfa8b7d3
quangtran666 <quangprox121@gmail.com>
2026-02-01 23:40:05 +0700

    Add conform.nvim, gdscript-formatter, godot integration

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+ return {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                gdscript = { "gdscript_formatter" },
+		lua = { "stylua" },
+            },
+            formatters = {
+                gdscript_formatter = {
+                    command = "gdscript-formatter",
+                    stdin = true,
+                },
+            },
+            format_on_save = {
+                timeout_ms = 1000,
+                lsp_fallback = false,
+            },
+          })
+
+        vim.keymap.set("n", "<leader>f", function()
+            require("conform").format({ async = true })
+        end, { desc = "Format buffer" })
+    end,
+}
